2bc034e48e28dc7aafcf27aa253605e2107e4e0d
Carl <carln@foxmail.com>
2021-03-31 20:33:35 +0100

    update

--- Practice/src/LeetCodeTest/linkedlist/LeetCode_2.java
+++ Practice/src/LeetCodeTest/linkedlist/LeetCode_2.java
@@ -7,25 +7,6 @@ package LeetCodeTest.linkedlist;
  */
 public class LeetCode_2 {
 
-    public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(4);
-        ListNode listNode2 = new ListNode(7);
-        ListNode listNode3 = new ListNode(5);
-
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-
-        ListNode listNode4 = new ListNode(9);
-        ListNode listNode5 = new ListNode(9);
-        ListNode listNode6 = new ListNode(9);
-
-        listNode4.next = listNode5;
-        listNode5.next = listNode6;
-        ListNodeService listNodeService = new ListNodeService();
-        listNodeService.list(LeetCode_2.addTwoNumbers(listNode1, listNode4));
-
-    }
-
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode temp = head;
