b7e130c00be972cd75deb03b61065e339833a96d
Carl <carln@foxmail.com>
2021-04-08 23:11:27 +0100

    update

--- Practice/src/LeetCodeTest/offer/tree/offer_07.java
+++ Practice/src/LeetCodeTest/offer/tree/offer_07.java
@@ -22,7 +22,7 @@ public class offer_07 {
 
     public TreeNode recur(int pre_root, int in_left, int in_right) {
         if (in_left > in_right) return null;
-        int index = map.get(preorder[pre_root]);
+        int index = map.get(preorder[pre_root]);    //获取中序遍历中对应前序遍历的根节点的位置索引
         TreeNode root = new TreeNode(preorder[pre_root]);
         root.left = recur(pre_root + 1, in_left, index - 1);
         root.right = recur(pre_root + (index - in_left) + 1, index + 1, in_right);
