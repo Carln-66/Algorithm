c535d1a53a9c557d25b1b5af85e43f2cc7432c78
Carl <carln@foxmail.com>
2021-04-12 10:44:04 +0100

    update

---
+++ Practice/src/LeetCodeTest/offer/linkedlist/offer_22.java
@@ -0,0 +1,24 @@
+package LeetCodeTest.offer.linkedlist;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/04/11/22:01
+ * @Description:  #22 链表中倒数第k个节点
+ */
+public class offer_22 {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head;
+        int len = 0;
+        while (temp.next != null) {
+            len++;
+            temp = temp.next;
+        }
+        for (int count = len - k + 1; count > 0; count--){
+            head = head.next;
+        }
+        return head;
+    }
+}
