b497c80aa7bccb1fd0d36471365cbf3bc6356a31
Carl <carln@foxmail.com>
2021-04-15 16:48:05 +0100

    update

---
+++ Practice/src/LeetCodeTest/offer/dynamic/offer_48.java
@@ -0,0 +1,24 @@
+package LeetCodeTest.offer.dynamic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/04/15/16:41
+ * @Description:  #48 最长不含重复字符的子字符串
+ */
+public class offer_48 {
+    //利用动态规划+哈希表
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int res = 0, temp = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int j = map.getOrDefault(s.charAt(i), -1);
+            map.put(s.charAt(i), i);
+            temp = temp < i - j ? temp + 1 : i - j;
+            res = Math.max(res, temp);
+        }
+        return res;
+    }
+}
