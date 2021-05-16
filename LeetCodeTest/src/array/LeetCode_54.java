9c30a3b7bac4ce15f0e5e977a9e0870b52e27400
Carl <carln@foxmail.com>
2021-05-16 21:18:56 +0100

    update

---
+++ LeetCodeTest/src/array/LeetCode_54.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/05/16/20:53
+ * @Description: 螺旋矩阵
+ */
+public class LeetCode_54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (top > bottom || left > right) break;
+            for (int i = right; i >= left; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return res;
+    }
+}
