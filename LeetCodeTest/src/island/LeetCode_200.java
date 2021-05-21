bb6daba9bc80df5c77736a819ce14cbd18467dea
Carl <carln@foxmail.com>
2021-05-21 12:45:27 +0100

    update

---
+++ LeetCodeTest/src/island/LeetCode_200.java
@@ -0,0 +1,36 @@
+package island;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/05/21/12:34
+ * @Description: 岛屿的数量
+ */
+public class LeetCode_200 {
+    //深度优先遍历
+    int res = 0;
+    public int numIslands(char[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int row, int column) {
+        if (row > grid.length || column > grid[0].length || row < 0 || column < 0) {
+            return;
+        }
+        if (grid[row][column] != '1') {
+            return;
+        }
+        grid[row][column] = '2';
+        dfs(grid, row - 1, column);
+        dfs(grid, row + 1, column);
+        dfs(grid, row, column - 1);
+        dfs(grid, row, column + 1);
+    }
+}
