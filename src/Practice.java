360ca5f4995b45d1dd8af9d5cceb8c1d6286e657
Carl <carln@foxmail.com>
2021-05-25 09:10:58 +0100

    update

---
+++ src/Practice.java
@@ -0,0 +1,27 @@
+/**
+ * @Auther: Carl
+ * @Date: 2021/05/25/8:57
+ * @Description:
+ */
+public class Practice {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return next;
+    }
+    public ListNode reverseList2(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
---
+++ src/utils.java
@@ -0,0 +1,26 @@
+/**
+ * @Auther: Carl
+ * @Date: 2021/05/25/8:58
+ * @Description:
+ */
+public class utils {
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
