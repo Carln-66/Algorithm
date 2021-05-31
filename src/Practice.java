45fb60b0ddacf78d114a35f5c2950a651f3d982f
Carl <carln@foxmail.com>
2021-05-31 15:39:31 +0100

    update

--- src/Practice.java
+++ src/Practice.java
@@ -363,6 +363,23 @@ public class Practice {
         }
         return ' ';
     }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int res = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            res++;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+        return res;
+    }
 }
 
 class CQueue {
