b1a77cc3e4fcd2d607b1b5a644a632c59beba1fc
Carl <carln@foxmail.com>
2021-05-31 14:19:00 +0100

    update

--- src/Practice.java
+++ src/Practice.java
@@ -301,6 +301,24 @@ public class Practice {
         }
         return profit;
     }
+
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
 }
 
 class CQueue {
--- src/Preview.java
+++ src/Preview.java
@@ -6,18 +6,16 @@
 public class Preview {
     public void BubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            boolean flag = true;
+            boolean  flag = true;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j];
+                    arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     flag = false;
                 }
             }
-            if (flag) {
-                break;
-            }
+            if (flag) break;
         }
     }
     public void quickSort(int[] arr, int left, int right) {
@@ -50,7 +48,7 @@ public class Preview {
 
     public void insertSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            int index = i - 1;
+            int index = i;
             int value = arr[i];
             while (index >= 0 && value < arr[index]) {
                 arr[index + 1] = arr[index];
@@ -60,14 +58,11 @@ public class Preview {
         }
     }
 
-    public int[] mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-        return arr;
+    public void mergeSort(int[] arr, int left, int right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 
     private static void merge(int[] arr, int left, int mid, int right) {
@@ -81,16 +76,46 @@ public class Preview {
             } else {
                 temp[k++] = arr[r++];
             }
+            while (l <= mid) {
+                temp[k++] = arr[l++];
+            }
+            while (r <= right) {
+                temp[k++] = arr[r++];
+            }
+            for (int i = 0; i < temp.length; i++) {
+                arr[i + left] = temp[i];
+            }
         }
+    }
 
-        while (l <= mid) {
-            temp[k++] = arr[l++];
+    public static void shellSort(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = 0; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                while (j - gap > 0 && temp < arr[j - gap]) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = temp;
+            }
         }
-        while (r <= right) {
-            temp[k++] = arr[r++];
-        }
-        for (int i = 0; i < temp.length; i++) {
-            arr[i + left] = temp[i];
+    }
+
+    public void selectSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            if (index != i) {
+                arr[index] = arr[i];
+                arr[i] = min;
+            }
         }
     }
 }
