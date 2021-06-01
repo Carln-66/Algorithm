7b06034ac4fd01a342aac234914c061ca1a6409f
Carl <carln@foxmail.com>
2021-06-01 14:31:22 +0100

    update

--- LeetCodeTest/src/offer/array/offer_57_ii.java
+++ LeetCodeTest/src/offer/array/offer_57_ii.java
@@ -1,4 +1,4 @@
-package offer;
+package offer.array;
 
 import java.util.ArrayList;
 
--- src/Practice.java
+++ src/Practice.java
@@ -394,6 +394,30 @@ public class Practice {
         }
         return left;
     }
+
+    public int[][] findContinuousSequence(int target) {
+        int left = 1, right = 1;
+        int sum = 0;
+        List<int[]> res = new ArrayList<>();
+        while (left <= target >> 1) {
+            if (sum < target) {
+                sum += right;
+                right++;
+            } else if (sum > target) {
+                sum -= left;
+                left++;
+            } else {
+                int[] arr = new int[right - left];
+                for (int i = left; i < right; i++) {
+                    arr[i - left] = i;
+                }
+                res.add(arr);
+                sum -= left;
+                left++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
 }
 
 class CQueue {
