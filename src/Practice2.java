812aa3dae3d0db8ca7b30912294873bfabc1745e
Carl <carln@foxmail.com>
2021-06-03 13:41:16 +0100

    update

--- src/Practice2.java
+++ src/Practice2.java
@@ -49,4 +49,29 @@ public class Practice2 {
         }
         return right - left - 1;
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum > 9 ? 1 : 0;
+            sum = sum % 10;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            temp.next = new ListNode(1);
+        }
+        return head.next;
+    }
 }
