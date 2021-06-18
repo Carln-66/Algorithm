393536208266202cfd8c2311556d67b58794cb58
Carl <carln@foxmail.com>
2021-06-18 19:57:37 +0100

    update

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode res = new ListNode(0);
-        res.next = head;
-        ListNode tempHead = head;
-        ListNode tempTail = head.next;
-        for (int i = 0; i < left - 1; i++) {
-            tempHead = tempHead.next;
-            tempTail = tempTail.next;
+    public List<String> letterCombinations(String digits) {
+        Map<Character, String> map = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        List<String> res = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return res;
+                recur(res, digits, map, new StringBuilder(), 0);
+        return res;
+    }
+
+    private void recur(List<String> res, String digits, Map<Character, String> map, StringBuilder sb, int level) {
+        if (level == digits.length()) {
+            res.add(sb.toString());
+            return;
         }
-        for (int i = 0; i < right - left; i++) {
-            ListNode temp = tempTail.next;
-            tempTail.next = tempTail.next.next;
-            tempHead.next = temp.next;
-            temp.next = tempTail;
+        char num = digits.charAt(level);
+        String s = map.get(num);
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            recur(res, digits, map, sb, level + 1);
+            sb.deleteCharAt(level);
         }
-        return res.next;
     }
 }
