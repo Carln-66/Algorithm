f57adbaeb921c590c94d479c4ba8d0d1efdd0c23
Carl <carln@foxmail.com>
2021-06-21 22:51:48 +0100

    update

--- LeetCodeTest/src/array/LeetCode_78.java
+++ LeetCodeTest/src/array/LeetCode_78.java
@@ -1,18 +1,26 @@
+package array;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Practice {
+/**
+ * @Auther: Carl
+ * @Date: 2021/06/21/22:45
+ * @Description: 子集
+ */
+public class LeetCode_78 {
+    //采用递归+回溯
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         backTrack(res, nums, 0, new ArrayList<>());
         return res;
     }
 
-    private void backTrack(List<List<Integer>> res, int[] nums, int layer, List<Integer> temp) {
+    private void backTrack(List<List<Integer>> res, int[] nums, int layer, ArrayList<Integer> temp) {
         res.add(new ArrayList<>(temp));
         for (int i = layer; i < nums.length; i++) {
             temp.add(nums[i]);
-            backTrack(res, nums,i + 1, temp);
+            backTrack(res, nums, i + 1, temp);
             temp.remove(temp.size() - 1);
         }
     }
