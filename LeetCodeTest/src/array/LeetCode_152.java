6d8881ff9bbe78b1e07bbd558d06e01a9e112fcc
Carl <carln@foxmail.com>
2021-07-06 13:38:34 +0100

    update

--- LeetCodeTest/src/array/LeetCode_152.java
+++ LeetCodeTest/src/array/LeetCode_152.java
@@ -1,6 +1,11 @@
-import java.util.*;
+package array;
 
-public class Main {
+/**
+ * @Auther: Carl
+ * @Date: 2021/07/06/13:38
+ * @Description: 乘积最大子数组
+ */
+public class LeetCode_152 {
     public int maxProduct(int[] nums) {
         int max = Integer.MIN_VALUE, imax = 1, imin = 1;
         for (int i = 0; i < nums.length; i++) {
