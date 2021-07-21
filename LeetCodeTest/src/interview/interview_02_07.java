49337e8312fd93ae15e1279fe2300c7f774091a0
Carl <carln@foxmail.com>
2021-07-21 21:24:44 +0100

    update

--- LeetCodeTest/src/interview/interview_02_07.java
+++ LeetCodeTest/src/interview/interview_02_07.java
@@ -1,18 +1,36 @@
 package interview;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @Auther: Carl
  * @Date: 2021/07/21/7:22
  * @Description: 链表相交
  */
 public class interview_02_07 {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode curA = headA;
-        ListNode curB = headB;
-        while (curA != curB) {
-            curA = curA == null ? headB : curA.next;
-            curB = curB == null ? headA : curB.next;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
         }
-        return curA;
+        return res;
     }
 }
