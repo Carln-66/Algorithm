b6855a98ee8501b36305d5b6d34490b7a6990d0a
Carl <carln@foxmail.com>
2021-09-03 18:16:23 +0800

    update

--- LeetCodeTest/src/offer/linkedlist/offer_35.java
+++ LeetCodeTest/src/offer/linkedlist/offer_35.java
@@ -1,16 +1,26 @@
+package offer.linkedlist;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
 
-public class Main {
+/**
+ * @Auther: Carl
+ * @Date: 2021/09/03/18:13
+ * @Description: 复杂链表的复制
+ * 利用映射表实现
+ */
+public class offer_35 {
     public Node copyRandomList(Node head) {
         Node cur = head;
         Map<Node, Node> map = new HashMap<>();
+        //构建原节点->新节点的映射关系
         while (cur != null) {
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
+        //返回头部
         cur = head;
+        //从map中获取值，重新构建新节点的属性映射
         while (cur != null) {
             map.get(cur).next = map.get(cur.next);
             map.get(cur).random = map.get(cur.random);
---
+++ src/Node.java
@@ -0,0 +1,16 @@
+/**
+ * @Auther: Carl
+ * @Date: 2021/09/03/17:54
+ * @Description:
+ */
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
