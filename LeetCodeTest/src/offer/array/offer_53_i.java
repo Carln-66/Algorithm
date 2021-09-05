810349b44a4283e4f5affe205faea3feff8aa373
Carl <carln@foxmail.com>
2021-09-05 13:12:53 +0800

    update

--- LeetCodeTest/src/offer/array/offer_53_i.java
+++ LeetCodeTest/src/offer/array/offer_53_i.java
@@ -35,4 +35,34 @@ public class offer_53_i {
             }
         }
     }
+
+    //两次二分
+    public int search1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int ptrR = left;
+        if (right >= 0 && nums[right] != target) {
+            return 0;
+        }
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int ptrL = right;
+        return ptrR - ptrL - 1;
+    }
 }
