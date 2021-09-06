9ce2fca0bf73d65e5aad0370f1096b40e2afd71b
Carl <carln@foxmail.com>
2021-09-06 17:05:39 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Semaphore;
-import java.util.function.IntConsumer;
-
 public class Main {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int x = 0;
+        int y = column - 1;
+        while (x < row && y >= 0) {
+            int num = matrix[x][y];
+            if (num == target) {
+                return true;
+            } else if (num < target){
+                x++;
             } else {
-                right = mid - 1;
+                y--;
             }
         }
-        int ptrR = left;
-        if (right >= 0 && nums[right] != target) {
-            return 0;
-        }
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        int ptrL = right;
-        return ptrR - ptrL - 1;
+        return false;
     }
 }
